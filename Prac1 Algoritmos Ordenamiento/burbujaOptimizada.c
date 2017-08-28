555b2edf0252ef06fb96b6eb4ae8fbc1f18339dc
Giselle <giselle.ft@hotmail.com>
2017-08-28 11:41:05 -0500

    agregue tiempos BO

--- Prac1 Algoritmos Ordenamiento/burbujaOptimizada.c
+++ Prac1 Algoritmos Ordenamiento/burbujaOptimizada.c
@@ -18,12 +18,15 @@ void burbujaOptimizada(int *arrNums, int n){
 			}
 		}
 	}
-	for(int i=0;i<n;i++){
-		printf("%d\n",arrNums[i]);	
-	}
+	//for(int i=0;i<n;i++){
+	//	printf("%d\n",arrNums[i]);	
+	//}
 }
 
+
 int main(int argc, char const *argv[]){
+	double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
+
 	/*Validamos primero que contengan mas de dos argumentos para verificar la n de entrada*/
 	if(argc>=2){
 		//utilizamos el segundo argumento como la n que va a introducir el usuario
@@ -37,9 +40,30 @@ int main(int argc, char const *argv[]){
 			scanf("%d",&arrNums[i]);
 		}
 
+		//Iniciar el conteo del tiempo para las evaluaciones de rendimiento
+		uswtime(&utime0, &stime0, &wtime0);
+
 		//ejecutamos la funcion
 		burbujaOptimizada(arrNums,n);
 
+		//Evaluar los tiempos de ejecución 
+		uswtime(&utime1, &stime1, &wtime1);
+
+		//Cálculo del tiempo de ejecución del programa
+		printf("\n");
+		printf("real (Tiempo total)  %.10f s\n",  wtime1 - wtime0);
+		printf("user (Tiempo de procesamiento en CPU) %.10f s\n",  utime1 - utime0);
+		printf("sys (Tiempo en acciónes de E/S)  %.10f s\n",  stime1 - stime0);
+		printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+		printf("\n");
+	
+		//Mostrar los tiempos en formato exponecial
+		printf("\n");
+		printf("real (Tiempo total)  %.10e s\n",  wtime1 - wtime0);
+		printf("user (Tiempo de procesamiento en CPU) %.10e s\n",  utime1 - utime0);
+		printf("sys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
+		printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+		printf("\n");
 
 	}else{
 		printf("Faltan argumentos\n");
