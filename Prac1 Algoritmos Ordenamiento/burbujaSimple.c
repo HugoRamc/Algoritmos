82485fd40b9fd081041cfaa0917d05ff4fbd9507
Giselle <giselle.ft@hotmail.com>
2017-08-28 11:39:23 -0500

    agregue tiempos BS

--- Prac1 Algoritmos Ordenamiento/burbujaSimple.c
+++ Prac1 Algoritmos Ordenamiento/burbujaSimple.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tiempo.h"
 
 //La funcion recibe como argumento un apuntador a arreglo de enteros, y el numero de elementos a ordenar maximo 10,000,000
 void burbujaSimple(int *arrNums, int n){
@@ -15,12 +16,14 @@ void burbujaSimple(int *arrNums, int n){
 		//printf("%d\n",arrNums[i-1]);	
 	}
 
-	for(int i=0;i<n;i++){
-		printf("%d\n",arrNums[i]);	
-	}
+	//for(int i=0;i<n;i++){
+	//	printf("%d\n",arrNums[i]);	
+	//}
 }
 
 int main(int argc, char const *argv[]){
+	double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
+
 	/*Validamos primero que contengan mas de dos argumentos para verificar la n de entrada*/
 	if(argc>=2){
 		//utilizamos el segundo argumento como la n que va a introducir el usuario
@@ -34,8 +37,31 @@ int main(int argc, char const *argv[]){
 			scanf("%d",&arrNums[i]);
 		}
 
+		//Iniciar el conteo del tiempo para las evaluaciones de rendimiento
+		uswtime(&utime0, &stime0, &wtime0);
+
 		//ejecutamos la funcion
 		burbujaSimple(arrNums,n);
+
+		//Evaluar los tiempos de ejecución 
+		uswtime(&utime1, &stime1, &wtime1);
+
+		//Cálculo del tiempo de ejecución del programa
+		printf("\n");
+		printf("real (Tiempo total)  %.10f s\n",  wtime1 - wtime0);
+		printf("user (Tiempo de procesamiento en CPU) %.10f s\n",  utime1 - utime0);
+		printf("sys (Tiempo en acciónes de E/S)  %.10f s\n",  stime1 - stime0);
+		printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+		printf("\n");
+	
+		//Mostrar los tiempos en formato exponecial
+		printf("\n");
+		printf("real (Tiempo total)  %.10e s\n",  wtime1 - wtime0);
+		printf("user (Tiempo de procesamiento en CPU) %.10e s\n",  utime1 - utime0);
+		printf("sys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
+		printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+		printf("\n");
+
 
 
 	}else{
@@ -43,4 +69,3 @@ int main(int argc, char const *argv[]){
 	}
 	return 0;
 }
-
