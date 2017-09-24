63e79a5e2e903510925997bbd442ccce0cc2e1c8
Giselle <giselle.ft@hotmail.com>
2017-09-24 15:23:11 -0500

    agregue tiempos busqBinaria

--- Practica2/busqBinaria.c
+++ Practica2/busqBinaria.c
@@ -1,16 +1,10 @@
-/*
-	Titulo: Algoritmo de busqueda binaria"
-	Descripción: Este programa recibe un dos numero enteros y un arreglo de enteros ordenados de manera ascendente del tamaño del segundo 
-				 entero introducido, el primer numero entero introducido será el mismo que se busque durante la ejecución de este algoritmo
-				 la busqueda binaria se realiza reduciendo el espacio de busqueda de los numeros, este algoritmo funciona solamente si los 
-				 números se encuentran ordenados.
-
-
+/*	Titulo: Algoritmo de busqueda binaria
+	Descripción: Este programa recibe dos numero enteros y un arreglo de enteros ordenados de manera ascendente del tamaño 				del segundo entero introducido
+		El primer numero entero introducido será el mismo que se busque durante la ejecución de este algoritmo
+		La busqueda binaria se realiza reduciendo el espacio de busqueda de los numeros, este algoritmo funciona 			solamente si los números se encuentran ordenados.
 	Fecha:23 - septiembre - 2017
 	Versión: 1.1
-	Autor: Hugo Enrique Ramírez Centeno 
-
-	
+	Autor: Hugo Enrique Ramírez Centeno 	
 */
 
 #include <stdio.h>
@@ -19,31 +13,29 @@
 
 //esta función recibe como parametro el arreglo de enteros, el tamaño del arreglo y el numero a buscar
 void busquedaBinaria(int *arr,int n,int numBusqueda){
-	int inferior=0;
-	int superior=n;
-	int centro;
-	int bandera = 0;
+	int inferior=0;//indice inferior
+	int superior=n;//indice superior
+	int centro;//indice mitad o centro
+	int bandera = 0;//para saber si encontro el numero
 
 	//
 	while((inferior <= superior)){
 		//obtenemos la posicion de la mitad del arreglo
 		centro = (inferior + superior)/2;
 
-		//verificamos si el arreglo en esa posicion es el indicado
+		//verificamos si el arreglo en esa posicion (centro o mitad) es el buscado 
 		if(arr[centro] == numBusqueda){
-			printf("numero encontrado: %d\n",arr[centro]);
+			printf("Numero %d encontrado en el arreglo\n",arr[centro]);
 			bandera = 1;
-			break;
+			break;//deja de buscar 
+		}//verificamos en que mitad puede estar el numero a buscar
+		else if(arr[centro] > numBusqueda){//se toma mitad izquierda por que el numero buscado es menor que el centro
+			superior = centro-1;//se modifica indice superior
 
-		//verificamos en que mitad puede estar el numero a buscar 
-		}else if(arr[centro] > numBusqueda){
-			superior = centro-1;
-
-		}else{
-			inferior = centro+1;
 		}
-		
-
+		else{//se toma mitad derecha por que el numero buscado es mayor que el centro
+			inferior = centro+1;//se modifica indice inferior
+		}
 	}
 
 	if(bandera == 0){
@@ -55,36 +47,51 @@ void busquedaBinaria(int *arr,int n,int numBusqueda){
 
 
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 	//validamos que el numero de parametros sean 
 	if(argc >= 3){
+		double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
+
 		int *arr;//variable para el arreglo de los numeros ordenados
 		int numBusqueda; //variable para almacenar el numero a buscar
-		int n;//variable para indicar el taamño del arreglo
+		int n;//variable para indicar el tamaño del arreglo
 
 		//asignacion de variables
 		n = atoi(argv[2]);
 		numBusqueda = atoi(argv[1]);
-		arr = (int *)malloc(sizeof(int)*n);
+		arr = (int *)malloc(sizeof(int)*n);//reservamos memoria para el tamaño del arreglo
 
+		//llenamos el arreglo, ya estan ordenados
 		for(int i=0;i<n;++i){
 			scanf("%d",&arr[i]);
 		}
 
-
+		//tiempo
+		uswtime(&utime0, &stime0, &wtime0);
+		//Se llama a la funcion de busqueda
 		busquedaBinaria(arr,n,numBusqueda);
+		//tiempo
+		uswtime(&utime1, &stime1, &wtime1);
 
-
-
-	}else{
-		printf("Faltan argumentos de ejecución\n");
+		//Cálculo del tiempo de ejecución del programa
+		printf("\n");
+		printf("real (Tiempo total)  %.10f s\n",  wtime1 - wtime0);
+		printf("user (Tiempo de procesamiento en CPU) %.10f s\n",  utime1 - utime0);
+		printf("sys (Tiempo en acciónes de E/S)  %.10f s\n",  stime1 - stime0);
+		printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+		printf("\n");	
+	
+		//Mostrar los tiempos en formato exponecial
+		printf("\n");
+		printf("real (Tiempo total)  %.10e s\n",  wtime1 - wtime0);
+		printf("user (Tiempo de procesamiento en CPU) %.10e s\n",  utime1 - utime0);
+		printf("sys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
+		printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+		printf("\n");
 
 	}
-
-
-
-
-
+	else{
+		printf("Faltan argumentos de ejecución\n");
+	}
 	return 0;
 }
